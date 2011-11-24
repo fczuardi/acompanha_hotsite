1481248564684022dc0bad518711508f0eeec0f4
Fabricio C Zuardi <fabricio@fabricio.org>
2011-11-24 09:55:27 -0200

    exemplo de svg que escala junto com a tela

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@
 
 */
 
+function windowHeightUpdated(){
+  var ww = window.innerWidth,
+      wh = window.innerHeight,
+      scaleW = ww/100,
+      scaleH = wh/100,
+      svg_stage = document.getElementById('svg-stage');
+
+  svg_stage.setAttribute('transform', 'scale('+scaleW+', '+scaleH+')');
+}
+window.addEventListener('load', windowHeightUpdated, true);
+window.addEventListener('resize', windowHeightUpdated, true);
 
 
 
