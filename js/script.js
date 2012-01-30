0096b88e1c158b40c97bfa20977c4268f2d376a8
Fabricio Zuardi <fabricio@fabricio.org>
2012-01-30 18:40:16 -0200

    atualizando a lista de pngs pro ie consertar

--- js/script.js
+++ js/script.js
@@ -174,7 +174,7 @@ function addBrowserClasses(){
 }
 function IETweaks(){
   if ($('html').hasClass('lt-ie7')){
-    DD_belatedPNG.fix('#assinatura, #linha_avaliacao, #linha_midiateca, #linha_aplicativos, #linha_licoes, #linha_ebooks, #linha_saraiva');
+    DD_belatedPNG.fix('#assinatura, #linha_avaliacao, #linha_midiateca, #linha_atividades_multimidia, #linha_saraiva_educa');
   }
 }
 function alturasMinimas(){
